e67879fa8903f8a2e9e67c1cd5b341330a2650f3
Pabasara Navod <144713553+Pabs-codes@users.noreply.github.com>
2024-02-21 16:19:29 +0530

    dfvb

--- src/configs/Configs.js
+++ src/configs/Configs.js
@@ -1,2 +1,4 @@
-export const API_URL = "http://localhost:8000";
+// export const API_URL = "http://localhost:8000";
 // export const API_URL = "https://smart-pet-feeder-backend.herokuapp.com";
+
+export const API_URL = "https://tame-pear-salmon-slip.cyclic.app";
